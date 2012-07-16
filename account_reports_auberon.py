980d19e33f43ee88d8999deb55f3787e311b07a1
Roxly Rivero <roxlyrivero@gmail.com>
2012-07-17 03:08:52 +0800

    Changes:
    Added the first class ('account_reports_auberon_gl')
    fields:
    	account_code, account_name, account_debit,
    	account_credit, journal_name
    No Functions included

--- account_reports_auberon.py
+++ account_reports_auberon.py
@@ -3,3 +3,20 @@
 #Auberon Solutions.com
 #
 #
+
+from osv import osv, fields
+
+
+class account_reports_auberon_gl(osv.osv):
+    _name = "account.reports.auberon.gl"
+    _description = "Accounting Reports by Auberon"
+    _columns = {
+        'account_code': fields.char('Account Code', size=64),
+        'account_name': fields.char('Account Name', size=64),
+        'account_debit': fields.char('Debit', size=64),
+        'account_credit': fields.char('Credit', size=64),
+        'journal_name':fields.char('Journal', size=64)
+    }
+    _order = "account_code"
+
+account_reports_auberon_gl()
